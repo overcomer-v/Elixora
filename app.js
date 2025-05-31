11de95cc100c6a2bea805801cf8cba475db3e4d3
Atoyeje Victor <atoyejeovercomer2@gmail.com>
2025-05-31 11:32:05 +0100

    update on navbar

--- app.js
+++ app.js
@@ -1,50 +1,66 @@
- const allButtons = document.querySelectorAll("button");
-        const questions = document.querySelectorAll(".question");
-        const navIcon = document.getElementById("nav-icon");
-        const navMenu = document.getElementById("nav-menu");
+const allButtons = document.querySelectorAll("button");
+const questions = document.querySelectorAll(".question");
+const navIcon = document.getElementById("nav-icon");
+const navMenu = document.getElementById("nav-menu");
 
-        questions.forEach((question) => {
-            question.addEventListener("click", () => {
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const currentContent = getComputedStyle(question)
+      .getPropertyValue("--q-after-content")
+      .trim();
 
-                const currentContent = getComputedStyle(question).getPropertyValue("--q-after-content").trim();
+    if (currentContent === "'+'") {
+      question.style.setProperty("--q-after-content", "'-'");
+    } else {
+      question.style.setProperty("--q-after-content", "'+'");
+    }
 
-                if (currentContent === "'+'") {
-                    question.style.setProperty("--q-after-content", "'-'");
-                } else {
-                    question.style.setProperty("--q-after-content", "'+'")
-                }
+    const answer = question.nextElementSibling;
+    answer.classList.toggle("visible");
+  });
+});
 
-                const answer = question.nextElementSibling;
-                answer.classList.toggle("visible");
-            });
-        });
+allButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    location.reload();
+  });
+});
 
-        allButtons.forEach((button) => {
-            button.addEventListener("click", () => {
-                location.reload();
-            })
-        });
+document.querySelectorAll(".about-us-btn").forEach((p) => {
+  p.addEventListener("click", () => {
+    document.querySelector("footer").scrollIntoView({ behavior: "smooth" });
+  });
+});
 
-        document.querySelectorAll(".header-options p").forEach((p) => {
-            p.addEventListener("click", () => {
-                document.querySelector("footer").scrollIntoView({ behavior: "smooth" });
-            })
-        });
+document.querySelectorAll(".product-clk").forEach((p) => {
+  p.addEventListener("click", () => {
+    navMenu.classList.remove("active");
 
-        document.querySelectorAll("footer p").forEach((p) => {
-            p.addEventListener("click", () => {
-                document.querySelector("header").scrollIntoView({ behavior: "smooth" });
-            })
-        });
+    document
+      .querySelector("#popular-product-sec")
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
 
-        navIcon.addEventListener("click", (event) => {
-            navMenu.classList.toggle("active");
-            event.stopPropagation();
-        });
+document.querySelectorAll("footer p").forEach((p) => {
+  p.addEventListener("click", () => {
+    navMenu.classList.remove("active");
 
-        document.addEventListener("click", (event) => {
-            if (!navMenu.contains(event.target) && !navIcon.contains(event.target)) {
-                navMenu.classList.remove("active");
-            }
+    document.querySelector("header").scrollIntoView({ behavior: "smooth" });
+  });
+});
 
-        });
+navIcon.addEventListener("click", (event) => {
+  navMenu.classList.toggle("active");
+  event.stopPropagation();
+});
+
+document.addEventListener("click", (event) => {
+  closeNav();
+});
+
+function closeNav() {
+  if (!navMenu.contains(event.target) && !navIcon.contains(event.target)) {
+    navMenu.classList.remove("active");
+  }
+}
